6eb4d67362ca5a024eee2bcc5a27c96d6d721710
Tom-wms <tomislav.adriatic@gmail.com>
2025-04-06 22:45:57 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const App = () => {
-  return <h1>🚀 Nimato WMS App is Working!</h1>;
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
